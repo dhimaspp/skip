0f69f0a5f49d60373061c545f70d564129427604
dhimaspp <atasbumi.id@gmail.com>
2020-08-31 02:09:35 +0700

    deno dit

--- deno/main.ts
+++ deno/main.ts
@@ -34,21 +34,33 @@
 // printLabel(myObj);
 
 //interface is name to build a structure of types
-interface SquareConfig {
-  color?: string;
-  width?: number;
-}
+// interface SquareConfig {
+//   color?: string;
+//   width?: number;
+// }
 
-function rumusPersegi(config: SquareConfig): { color: string; area: number } {
-  let newSquare = { color: "white", area: 100 };
-  if (config.color) {
-    newSquare.color = config.color;
-  }
-  if (config.width) {
-    newSquare.area = config.width * config.width;
-  }
-  return newSquare;
-}
+// function rumusPersegi(config: SquareConfig): { color: string; area: number } {
+//   let newSquare = { color: "white", area: 100 };
+//   if (config.color) {
+//     newSquare.color = config.color;
+//   }
+//   if (config.width) {
+//     newSquare.area = config.width * config.width;
+//   }
+//   return newSquare;
+// }
 
-let mySquare = rumusPersegi({ color: "white", width: 10 });
-console.log(mySquare);
+// let mySquare = rumusPersegi({ color: "white", width: 10 });
+// console.log(mySquare);
+
+import { serve } from "https://deno.land/std@0.67.0/http/server.ts";
+import "https://deno.land/x/dotenv/mod.ts";
+
+const PORT = Deno.env.get("PORT") || 8080;
+const s = serve(`0.0.0.0:${PORT}`);
+const body = new TextEncoder().encode("Hello, Deno\n");
+
+console.log(`Server started on port ${PORT}`);
+for await (const req of s) {
+  req.respond({ body });
+}
